df015b7edc3b1bf4eb4243821c0bb8d430059782
Mattias Eriksson <thematerik@gmail.com>
2014-07-12 10:19:38 -0700

    added a test to make sure you can specify fields

--- test/tests/foo.js
+++ test/tests/foo.js
@@ -49,6 +49,62 @@ exports.testReadManyExpand = function(test) {
                         var foo = json.foos[i];
                         test.ok(foo.id);
                         test.ok(foo.name);
+                        test.ok(foo.user);
+                    }
+                    test.ok(json.hrefs.current);
+                    test.done();
+                });
+            });
+        });
+    });
+};
+
+exports.testReadManyFields = function(test) {
+    testlib.createUser(EMAIL, PASSWORD, function(userId) {
+        var d1 = {name: 'test1'};
+        var d2 = {name: 'test2'};
+        var qs = '?expand=foo&fields=name';
+        var path = '/users/' + userId + '/foos' + qs;
+        testlib.requests.post(path, d1, function(code, json) {
+            test.equal(code, httpStatus.CREATED);
+            testlib.requests.post(path, d2, function(code, json) {
+                test.equal(code, httpStatus.CREATED);
+                testlib.requests.get(path, function(code, json) {
+                    test.equal(code, httpStatus.OK);
+                    test.equal(json.foos.length, 2);
+                    for (var i in json.foos) {
+                        var foo = json.foos[i];
+                        test.ok(foo.id);
+                        test.ok(foo.name);
+                        test.ok(!foo.user);
+                    }
+                    test.ok(json.hrefs.current);
+                    test.done();
+                });
+            });
+        });
+    });
+};
+
+exports.testReadManyFieldsNested = function(test) {
+    testlib.createUser(EMAIL, PASSWORD, function(userId) {
+        var d1 = {name: 'test1'};
+        var d2 = {name: 'test2'};
+        var qs = '?expand=foo,user&fields=user,username';
+        var path = '/users/' + userId + '/foos' + qs;
+        testlib.requests.post(path, d1, function(code, json) {
+            test.equal(code, httpStatus.CREATED);
+            testlib.requests.post(path, d2, function(code, json) {
+                test.equal(code, httpStatus.CREATED);
+                testlib.requests.get(path, function(code, json) {
+                    test.equal(code, httpStatus.OK);
+                    test.equal(json.foos.length, 2);
+                    for (var i in json.foos) {
+                        var foo = json.foos[i];
+                        test.ok(foo.id);
+                        test.ok(foo.user);
+                        test.ok(foo.user.username);
+                        test.ok(!foo.user.email);
                     }
                     test.ok(json.hrefs.current);
                     test.done();
