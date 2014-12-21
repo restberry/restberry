6a7d683992e221335e9e4e4c0d50f802ee721d41
Mattias Eriksson <thematerik@gmail.com>
2014-12-21 05:23:10 -0800

    added a async test for making sure restberry handles multiple requests at once

--- tests/express-mongoose/tests/foo.js
+++ tests/express-mongoose/tests/foo.js
@@ -118,3 +118,47 @@ exports.testReadManyFieldsNested = function(test) {
         });
     });
 };
+
+exports.testReadManyAsync = function(test) {
+    var i = 0, n = 3;
+    var done = function() {
+        i++; if (i === n)  test.done();
+    };
+    testlib.requests.post('/bars', {name: 'x'}, function(code, json) {
+        test.equal(code, httpStatus.CREATED);
+        var barId = json.bar.id;
+        var path = '/bars/' + barId + '/foos';
+        testlib.requests.post(path, {name: 'x'}, function(code, json) {
+            test.equal(code, httpStatus.CREATED);
+            var fooId = json.foo.id;
+            var path = '/foos/' + fooId;
+            var path1 = path + '?fields=id';
+            testlib.requests.get(path1, function(code, json) {
+                test.equal(code, httpStatus.OK);
+                test.ok(!json.foo.name);
+                test.ok(!json.foo.bar);
+                done();
+            });
+            setTimeout(function() {
+                var path2 = path + '?expand=foo';
+                testlib.requests.get(path2, function(code, json) {
+                    test.equal(code, httpStatus.OK);
+                    test.ok(json.foo.name);
+                    test.ok(json.foo.bar);
+                    test.ok(!json.foo.bar.name);
+                    done();
+                });
+                setTimeout(function() {
+                    var path3 = path + '?expand=foo,bar';
+                    testlib.requests.get(path3, function(code, json) {
+                        test.equal(code, httpStatus.OK);
+                        test.ok(json.foo.name);
+                        test.ok(json.foo.bar);
+                        test.ok(json.foo.bar.name);
+                        done();
+                    });
+                }, 10);
+            }, 10);
+        });
+    });
+};
